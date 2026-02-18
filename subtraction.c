7a56f8f53fc6bc389f3875ef7da12b28622ef296
prajwal-cybersec <prajwalbn2007@gmail.com>
2026-02-18 22:06:17 +0530

    Add files via upload

---
+++ subtraction.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sub;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    sum = a - b;
+
+    printf("Sub = %d", sub);
+
+    return 0;
+}
