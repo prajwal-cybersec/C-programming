4c4c915fa0af94233c965c47e4d706ae4a80f66a
prajwal-cybersec <prajwalbn2007@gmail.com>
2026-02-19 17:23:52 +0530

    Add arithmetic operations in C program

---
+++ Day-3/Arthematic operator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 12, b = 5;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n\n", b);
+
+    printf("Addition (+) = %d\n", a + b);
+    printf("Subtraction (-) = %d\n", a - b);
+    printf("Multiplication (*) = %d\n", a * b);
+    printf("Division (/) = %d\n", a / b);
+    printf("Modulus (%%) = %d\n", a % b);
+
+    return 0;
+}
